7ce3963f1bce36072db2c8e5b3e377db7b084238
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2014-03-02 16:29:25 -0500

    Refactor JS.

--- vis/invisdemo.js
+++ vis/invisdemo.js
@@ -1,104 +1,74 @@
-invisLayout();
+$(document).ready(function() {
+  invisLayout();
 
-//load the rna world
-var rnaworld = new RNAWorld("data/invisdemo.json");
+  // Shared state
+  var rnaworld = new RNAWorld("data/invisdemo.json");
+  var canvas = d3.select("#canvas");
+  var pb = d3.select("#panel-bottom");
+  var pr = d3.select("#panel-right")
 
-//create views & visualizations for the rna world
-var canvas = d3.select("#canvas");
-var vis1   = new RNAWorldScatterplot(rnaworld,canvas);
-var legend = new Legend(vis1, d3.select("#panel-right"));
+  // Vis 1: RNAWorldScatterplot, a 2-dimensional embedding of RNA sequences
+  var vis1   = new RNAWorldScatterplot(rnaworld, canvas);
+  var legend = new Legend(vis1, pr);
 
-//add interactions
-legend.addToggleInteraction();
+  // Vis 2: Heatmap of moused-over RNA
+  var opts = newOpts();
+  opts.pad.left = parseFloat(canvas.style("left"));
+  opts.size.w = parseFloat(canvas.style("width"));
+  opts.size.h = 16;
 
-//mouse
-vis1.addMouseClick();
+  var seq = d3.range(rnaworld.refseq_.length);
+  var vis2 = new Heatmap(seq, pb, opts);
+  vis2.addRowLabels([" "]);
 
-//brush
-vis1.addBrush();
+  var lopts = newOpts();
+  lopts.pos.x = opts.pad.left+opts.size.w + 5;
+  lopts.pos.y = opts.pos.y;
+  lopts.size.h = 8 * opts.size.h;
+  lopts.size.w = parseInt(pr.style("width"));
+  lopts.icon = {w: 6, h: 16};
+  lopts.type = 0;
 
-d3.select("#refseq-button")
-  .on("click", function() {
-    if (d3.select(this).classed("active")) {
-      vis1.hideRefseq();
-      d3.select(this).classed("active", false);
-    } else {
-      d3.select(this).classed("active", true);
-      vis1.showRefseq();
-    }
+  var legend2 = new Legend(vis2, pb, lopts);
+  legend2.marks_.style("stroke", "none");
+  legend2.marks_.style("font-size", "4px");
+
+  // Vis 3: Heatmap of reference
+  opts.pos.y = 16;
+  var vis3 = new Heatmap(rnaworld.refseq_, pb, opts);
+  vis3.addRowLabels(["reference"]);
+
+  // Vis 4: RNAPopStackedBar for each experiment
+  opts.pos.y = 60;
+  opts.pos.x = 0;
+  var vis4 = new RNAWorldStatvis(rnaworld, pb, opts)
+
+  /**************
+   * INTERACTIONS
+  ***************/
+  legend.addToggleInteraction();
+
+  vis1.addMouseClick();
+  vis1.addBrush();
+  vis1.updateLinkToHeatmap(vis2);
+  $("button#export").click(function(event) { $("#file-browse").click(); });
+  $("#refseq-button").click(function() {
+    $(this).hasClass("active") ? vis1.hideRefseq() : vis1.showRefseq();
+    $(this).toggleClass("active");
   });
-
-d3.select("#path-button")
-  .on("click", function() {
-    vis1.shortestPath();
-  });
-
-//filter
-d3.select("#filter")
-  .on("change", function(d) {
-    d3.select("#filter-label")
-      .text("refseq:"+this.value);
+  $("#path-button").click(function() { vis1.shortestPath(); });
+  $("#filter").change(function() {
+    $("#filter-label").text("refseq:" + this.value);
     vis1.filter(this.value);
   });
-
-//filter
-d3.select("#enet")
-  .on("change", function(d) {
-                  d3.select("#enet-label").text("enet:" + this.value);
-                  vis1.epsilonFilter(this.value);
-                });
-
-var aggregateAND = d3.select("#aggregate-and");
-var aggregateOR  = d3.select("#aggregate-or");
-var  aggregateNOT = d3.select("#aggregate-not");
-
-aggregateAND.on("change", function() { vis1.aggregation(true,"and"); });
-aggregateOR.on("change", function() { vis1.aggregation(true,"or"); });
-
-
-//aggregation
-d3.select("#aggregate-reset")
-  .on("click", function() {
-                 vis1.aggregation(false);
-               });
-
-var bp = d3.select("#panel-bottom");
-var lp = d3.select("#panel-left");
-
-var opts = newOpts();
-opts.pad.left = parseFloat(canvas.style("left"));
-opts.size.w = parseFloat(canvas.style("width"));
-opts.size.h = 16;
-
-var seq = d3.range(rnaworld.refseq_.length);
-var vis2 = new Heatmap(seq, bp, opts);
-vis2.addRowLabels([" "]);
-
-var pb = d3.select("#panel-bottom");
-var lopts = newOpts();
-lopts.pos.x = opts.pad.left+opts.size.w+5;
-lopts.pos.y = opts.pos.y;
-lopts.size.h = 8 * opts.size.h;
-lopts.size.w = parseInt(d3.select("#panel-right").style("width"));
-lopts.icon = {w:6, h:16};
-lopts.type = 0;
-
-var legend2 = new Legend(vis2, pb, lopts);
-legend2.marks_.style("stroke", "none");
-legend2.marks_.style("font-size", "4px");
-opts.pos.y = 16;
-
-var vis3 = new Heatmap(rnaworld.refseq_, bp, opts);
-vis3.addRowLabels(["reference"]);
-vis3.linkToScatterplot(vis1);
-vis1.updateLinkToHeatmap(vis2);
-
-opts.pos.y = 60;
-opts.pos.x = 0;
-var vis4 = new RNAWorldStatvis(rnaworld,bp,opts)
-
-$(document).ready(function() {
-  $("button#export").click(function(event) {
-    document.getElementById('file-browse').click();
+  $("#enet").change(function() {
+    $("#enet-label").text("enet:" + this.value);
+    vis1.epsilonFilter(this.value);
   });
+  // TODO(hammer): fix the logic for this form
+  $("#aggregate-and").change(function() { vis1.aggregation(true, "and"); });
+  $("#aggregate-or").change(function() { vis1.aggregation(true, "or"); });
+  $("#aggregate-reset").click(function() { vis1.aggregation(false); });
+
+  vis3.linkToScatterplot(vis1);
 });
